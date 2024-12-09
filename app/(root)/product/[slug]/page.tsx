916e563d5605bed321368fad68c271f24f449ce4
Byoungcheon Lee <lbcsultan@gmail.com>
2024-12-09 16:39:26 +0900

    12. implement add to cart

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -7,6 +7,9 @@ import { Card, CardContent } from '@/components/ui/card'
 import { getProductBySlug } from '@/lib/actions/product.actions'
 import { APP_NAME } from '@/lib/constants'
 import { Button } from '@/components/ui/button'
+import AddToCart from '@/components/shared/product/add-to-cart'
+import { getMyCart } from '@/lib/actions/cart.actions'
+import { round2 } from '@/lib/utils'
 
 export async function generateMetadata({
   params,
@@ -31,6 +34,8 @@ const ProductDetails = async ({
 }) => {
   const product = await getProductBySlug(slug)
   if (!product) notFound()
+
+  const cart = await getMyCart()
 
   return (
     <>
@@ -84,7 +89,17 @@ const ProductDetails = async ({
                 </div>
                 {product.stock !== 0 && (
                   <div className=" flex-center">
-                    <Button className="w-full">Add to cart</Button>
+                    <AddToCart
+                      cart={cart}
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: round2(product.price),
+                        qty: 1,
+                        image: product.images![0],
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
--- types/index.ts
+++ types/index.ts
@@ -13,8 +13,15 @@
 //   banner?: string
 // }
 
-import { products } from '@/db/schema'
+import { carts, products } from '@/db/schema'
+import { cartItemSchema } from '@/lib/validator'
 import { InferSelectModel } from 'drizzle-orm'
+import { z } from 'zod'
 
 // PRODUCTS
 export type Product = InferSelectModel<typeof products>
+
+export type CartItem = z.infer<typeof cartItemSchema>
+
+// CART
+export type Cart = InferSelectModel<typeof carts>
